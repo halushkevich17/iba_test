5303ef336ba6f482374294c87432c6fd610516f5
halushkevich17 <84095411+halushkevich17@users.noreply.github.com>
2021-05-16 00:45:46 +0300

    Add files via upload

---
+++ Task 1, 2, 3.py
@@ -0,0 +1,50 @@
+import numpy
+import random
+
+"""задание 1 вариант 16"""
+# k = int(input("Ведите число от '0'до '7'"))
+# if k == 1:
+#     print("один")
+# if k == 2:
+#     print("два")
+# if k == 3:
+#     print("три")
+# if k == 4:
+#     print("четыре")
+# if k == 5:
+#     print("пять")
+# if k == 6:
+#     print("шесть")
+# if k == 7:
+#     print("семь")
+# if k == 0:
+#     print("ноль")
+# else:
+#     print("Ведено неверное значение")
+
+
+"""задание 3 вариант 15"""
+# list = [random.randint(1, 10) for number in range(int(input("Введите число списка")))]
+# print(list)
+# for number in list:
+#     if number % 3 == 0:
+#         list.remove(number)
+# print(list)
+
+
+"""задание 2 вариант 1"""
+# def sum1(number):
+#     summa = 0
+#     while number > 0:
+#         summa += number % 10
+#         number = number // 10
+#     return summa
+#
+# for i in range (100000, 999999):
+#     if sum1(i) % 7 == 0 and sum1(i + 1) % 7 == 0:
+#         print(str(i) + ' and ' + str(i + 1))
+
+
+
+
+
